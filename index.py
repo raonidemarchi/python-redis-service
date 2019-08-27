241dca74a604bed08214edde1a55ce9b0d31de80
Raoni Demarchi <raonidemarchi@gmail.com>
2019-08-27 16:22:25 -0300

    Add redis

--- index.py
+++ index.py
@@ -1,11 +1,28 @@
 from flask import Flask, request, jsonify
+import redis
+import sys
+import random
 
 app = Flask(__name__)
+redis = redis.Redis('redis')
 
-@app.route('/')
-def hello():
+redis.set('last-request', 'teste')
+
+@app.route('/<username>/<password>')
+def hello(username, password):
+  redis.set('username', username)
+  redis.set('token', random.randint(20000, 1000000))
+
   return {
-    'hello': 'world'
+    'hello': 'foi'
+  }
+
+@app.route('/validateAuth')
+def hello2():
+  result = 'Not Authenticated' if redis.get('username') == None else redis.get('token').decode('utf-8')
+
+  return {
+    'result': result
   }
 
 if __name__ == '__main__':
